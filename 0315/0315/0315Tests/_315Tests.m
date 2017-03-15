c8e20a0f083d7628eadd94c450c3c7a8d2240b4a
fjn-user-02 <chinh-tc@fujinet.net>
2017-03-15 10:45:06 +0700

    create base source

---
+++ 0315/0315/0315Tests/_315Tests.m
@@ -0,0 +1,39 @@
+//
+//  _315Tests.m
+//  0315Tests
+//
+//  Created by FJN DEV on 3/15/17.
+//  Copyright © 2017 0315. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _315Tests : XCTestCase
+
+@end
+
+@implementation _315Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
