a8f3b4168ecf1fe52b07ee69da41088f697c969b
fjn-user-02 <fjndev@ITs-Mac-mini-2.local>
2017-03-15 10:52:26 +0700

    add base source

---
+++ 0315/0315/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0315
+//
+//  Created by FJN DEV on 3/15/17.
+//  Copyright © 2017 0315. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
